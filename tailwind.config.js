eabe2346b4011c9acbe094d2a4a6f221503e389b
izumuri1 <izumuri@gmail.com>
2025-06-01 14:25:55 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}', './**/*.html'], // TailwindCSSが監視する対象ファイル
+  theme: {
+    extend: {}
+    }, // テーマの拡張設定
+  plugins: [], // 必要に応じてプラグインを追加
+};
